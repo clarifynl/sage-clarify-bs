f38cc41d13bc2dcc69ad36f79f32c45515558a28
Twansparant <toinekamps@gmail.com>
2023-02-15 15:26:51 +0100

    Add tailwind ignore rule

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,20 @@
 module.exports = {
 	extends: ['@roots/sage/stylelint-config'],
 	rules: {
-		'color-no-invalid-hex': true
+		'color-no-invalid-hex': true,
+		'indentation': 'tab',
+		'max-line-length': 250,
+		'no-invalid-position-at-import-rule': [
+			true,
+			{
+				ignoreAtRules: ['tailwind'],
+			}
+		],
+		'at-rule-no-unknown': [
+			true,
+			{
+				ignoreAtRules: ['tailwind'],
+			}
+		]
 	}
 }
