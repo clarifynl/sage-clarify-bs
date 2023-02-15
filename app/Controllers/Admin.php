0a959d18c810c910c81450c270201f3f7bd56b37
Twansparant <toinekamps@gmail.com>
2023-02-15 14:45:21 +0100

    Update function name

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -32,13 +32,13 @@ class Admin
 	public static function remove_add_new_post_admin_bar() {
 		?>
 		<script type="text/javascript">
-			function remove_add_new_post_href_in_admin_bar() {
+			function remove_add_new_post_admin_bar() {
 				var add_new = document.getElementById('wp-admin-bar-new-content');
 				if(!add_new) return;
 				var add_new_a = add_new.getElementsByTagName('a')[0];
 				if(add_new_a) add_new_a.setAttribute('href','#!');
 			}
-			remove_add_new_post_href_in_admin_bar();
+			remove_add_new_post_admin_bar();
 		</script>
 		<?php
 	}
