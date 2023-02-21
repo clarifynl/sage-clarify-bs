63ab34d6af2fb9077db482bf8fe45267a2cb0e9d
Twansparant <toinekamps@gmail.com>
2023-02-21 13:54:48 +0100

    Add override for .jsx files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,21 @@
 module.exports = {
 	root: true,
-	extends: ['@clarifynl/eslint-config-clarify'],
+	extends: [
+		'@clarifynl/eslint-config-clarify'
+	],
 	globals: {
-		'jQuery': 'readonly'
-	}
+		jQuery: 'readonly'
+	},
+	overrides: [
+		{
+			files: [ '**/*.jsx' ],
+			extends: [
+				'plugin:@wordpress/eslint-plugin/recommended',
+				'@clarifynl/eslint-config-clarify/react'
+			],
+			rules: {
+				'react-hooks/rules-of-hooks': 'warn'
+			}
+		}
+	]
 }
