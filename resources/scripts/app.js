370a2e98cb6a7503ca5d4ba224e712ca14424620
Twansparant <toinekamps@gmail.com>
2023-02-23 14:12:45 +0100

    Order imports

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,13 +1,13 @@
+import domReady from '@roots/sage/client/dom-ready';
 import 'bootstrap/js/src/util';
 import 'bootstrap/js/src/collapse';
 import 'bootstrap/js/src/dropdown';
 import 'bootstrap/js/src/modal';
 import 'bootstrap/js/src/tab';
 import 'lazysizes';
-
 import '@lottiefiles/lottie-player';
+
 import '@scripts/utils/faIcons';
-import domReady from '@roots/sage/client/dom-ready';
 import LottieInteractive from '@scripts/common/lottieInteractive';
 import CookieBar from '@scripts/sections/cookieBar';
 
