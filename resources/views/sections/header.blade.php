41dd4d346ba5f5b6da3c1bd55f1ae584ba1acaec
Twansparant <toinekamps@gmail.com>
2023-02-24 09:42:40 +0100

    Add default navbar toogle

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -3,12 +3,21 @@
 		<a class="header__brand" href="{{ $home_url }}" title="{!! $site_name !!}">
 			@svg('images.logo-placeholder', 'header__logo')
 		</a>
-		<button class="header__toggle js-hamburger-button" type="button" aria-label="{!! __('Toggle navigation', 'sage') !!}">
-			<span class="header__menu">{!! __('Menu', 'sage') !!}</span>
-		</button>
-		<div class="header__nav">
+		<div id="navMain" class="header__nav collapse">
 			@include('partials.navigation')
 		</div>
+		<button
+			class="header__toggle"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navMain"
+			aria-controls="navMain"
+			aria-expanded="false"
+			aria-label="{!! __('Toggle navigation', 'sage') !!}"
+		>
+			<span class="header__menu">{!! __('Menu', 'sage') !!}</span>
+			<span class="header__icon navbar-toggler-icon"></span>
+		</button>
 	</div>
 </header>
 
